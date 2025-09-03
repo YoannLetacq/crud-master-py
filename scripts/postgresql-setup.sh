eee2f2ed213a4fd8b7317560080b7950b29fb33a
YoannLetacq <yoann.letacq0@gmail.com>
2025-09-03 14:56:27 +0200

    feat: améliorer le script d'installation et de configuration de PostgreSQL

--- scripts/postgresql-setup.sh
+++ scripts/postgresql-setup.sh
@@ -1,32 +1,94 @@
 #!/bin/bash
+set -euo pipefail
+set -a 
+source .env
+set +a
 
-# install postgresql
-apt-get update && apt-get install -y postgresql-14 postgresql-contrib
+echo "Starting postgreSQL installation and set up..."
 
-# stop postgresql
-/etc/init.d/postgresql stop
+# update packages
+apt update -y 
 
-# Clear the DB to make it ready for setup
-rm -rf /var/lib/postgresql/14/main
+# install PostgreSQL
+if psql --version >/dev/null 2>&1; then 
+    echo "PostgreSQL is already present : $(psql --version)"
+else
+    echo "Installing PostgreSQl..."
+    apt install -y postgresql 
+    echo " PostgreSQL installed."
 
-# Init the database
-mkdir /var/lib/postgresql/14/main
-chown postgres:postgres /var/lib/postgresql/14/main
-cd /var/lib/postgresql/14/main
+    # install postgre-contrib (extensions and additions)
+    apt install -y postgresql-contrib
+fi 
 
-su -c '/usr/lib/postgresql/14/bin/initdb -D /var/lib/postgresql/14/main/' -s /bin/sh postgres
+# save the version installed
+PG_version=$(psql -V | awk '{print $3}' | cut -d '.' -f1)
+
+
+# Delete default cluster
+if pg_lsclusters | awk '{print $1, $2}' | grep -q "^${PG_version} main$"; then 
+    echo "Deletion of existing clusters..."
+    pg_dropcluster --stop ${PG_version} main
+fi
+
+
+# stop postgre before set up
+if systemctl is-active --quiet postgresql; then 
+    echo "PostgreSQL service is running, stopping it now..."
+    systemctl stop postgresql
+    echo "PostgreSQL service correctly stopped."
+else 
+    echo "PostgreSQL service already stopped."
+fi
+
+# Reconstruc the postgreSQL cluster
+echo "Reconstructing the cluster..."
+
+# Create cluster 
+echo "Creating new cluster..."
+pg_createcluster ${PG_version} main --start
+if ! pg_lsclusters | awk '$1 == "'${PG_version}'" && $2 == "main" {found=1} END{exit !found}'; then
+    echo "Cluster creation failed!"
+    exit 1
+fi
+
+
+PG_conf_dir="/etc/postgresql/${PG_version}/main"
 
 # Enable public access
-echo "listen_addresses='*'" >> /var/lib/postgresql/14/main/postgresql.conf
+if ! grep -q "listen_addresses='*'" "${PG_conf_dir}/postgresql.conf"; then
+    echo "listen_addresses='*'" >> "${PG_conf_dir}/postgresql.conf"
+fi
 
-# Enable public access
-echo "host  all  all 0.0.0.0/0 md5" >> /var/lib/postgresql/14/main/pg_hba.conf
+# enable public access
+if ! grep -q "host  all  all  0.0.0.0/0  md5" "${PG_conf_dir}/pg_hba.conf"; then
+    echo "host  all  all  0.0.0.0/0  md5" >> "${PG_conf_dir}/pg_hba.conf"
+fi
 
-# run postgresql server
-su -c 'nohup /usr/lib/postgresql/14/bin/pg_ctl -D /var/lib/postgresql/14/main -l logfile start'
+echo "Cluster recreate successfully and correctly reconfigure."
 
-/etc/init.d/postgresql start
+# restart postgre server after configuration
+systemctl restart postgresql
+if systemctl is-active --quiet postgresql; then
+    echo "PostgreSQL correctly running."
+else
+    echo "Something has gone wrong, PostgreSQL not running."
+    exit 1
+fi
 
-# setup the database
-sudo -u postgres -H -- psql -d postgres -c "CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}';"
-sudo -u postgres -H -- psql -d postgres -c "CREATE DATABASE ${DB_NAME} OWNER ${DB_USER};"
+# Setup the database and user
+sudo -u postgres psql -d postgres <<EOF
+DO
+\$body\$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '${DB_USER}') THEN
+      CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}';
+   END IF;
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DB_NAME}') THEN
+      CREATE DATABASE ${DB_NAME} OWNER ${DB_USER};
+   END IF;
+END
+\$body\$;
+EOF
+
+echo "Database and user setup complete!"
